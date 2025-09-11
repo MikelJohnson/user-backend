af3531e09a46e0f0d11569ea96c44b0d6b7ef441
mj <mikel5764@gmail.com>
2025-09-11 18:21:02 +0300

    Add user GET

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ await mongoose
 
 app.use(express.json());
 
-///...
+// Get user
+app.get("/user/:id", async (req, res) => {
+  try {
+    const users = await User.findById(req.params.id);
+    res.send(users);
+  } catch (err) {
+    res.status(404).send({"error":"User not found"});
+  }
+});
+
+app.all('/{*any}', async (req, res) => {
+    res.status(400).send({"error": `Cannot ${req.method} /${req.params.any??''}`})
+})
 
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
